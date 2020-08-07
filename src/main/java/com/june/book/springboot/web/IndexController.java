2b89437034b3b38f1f3afe377458238204dd257b
Highjune <Highjune37@gmail.com>
2020-08-07 15:26:01 +0900

    글 등록까지.

---
+++ src/main/java/com/june/book/springboot/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.june.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index"; // src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
