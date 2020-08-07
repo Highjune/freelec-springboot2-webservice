b6d760c69d4e1992787df66fb02e717d914eb5f6
Highjune <Highjune37@gmail.com>
2020-08-08 00:46:01 +0900

    fixed untracked files

---
+++ src/main/java/com/june/book/springboot/service/PostsService.java
@@ -0,0 +1,58 @@
+package com.june.book.springboot.service;
+
+import com.june.book.springboot.domain.posts.Posts;
+import com.june.book.springboot.domain.posts.PostsRepository;
+import com.june.book.springboot.web.dto.PostsListResponseDto;
+import com.june.book.springboot.web.dto.PostsResponseDto;
+import com.june.book.springboot.web.dto.PostsSaveRequestDto;
+import com.june.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id= " + id));
+
+        return new PostsResponseDto(entity); //PostsResponseDto 생성만 해주고 entity 넘겨줌
+    }
+
+    @Transactional(readOnly = true) //조회기능만 있는 경우는 이런식으로 readonly=true로 주면 조회속도가 개선(조회 기능만 갖고 있으므로)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAll().stream()
+                .map(PostsListResponseDto::new) //람다임 .map(posts -> new PostsListResponseDto(posts))와 같다
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
+
+}
