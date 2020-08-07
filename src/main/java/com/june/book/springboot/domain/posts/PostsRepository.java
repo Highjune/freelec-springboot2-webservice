751f690851d55285ebe3588cc079cca957fce0f0
Highjune <Highjune37@gmail.com>
2020-08-08 00:46:01 +0900

    fiixed untracked files, google login/passwd writed at application-oauth.properties

---
+++ src/main/java/com/june/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,13 @@
+package com.june.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+//SpringDataJpa에서 기본적으로 제공하지 않는 것들은 이렇게 @query로 쓸 수 있다.
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+}
+
