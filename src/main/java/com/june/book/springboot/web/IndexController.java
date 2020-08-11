3ccbbf422fd3771d79dd4cff44c85760dae29d92
Highjune <Highjune37@gmail.com>
2020-08-11 16:11:49 +0900

    구글 로그인 oauth 까지.

--- src/main/java/com/june/book/springboot/web/IndexController.java
+++ src/main/java/com/june/book/springboot/web/IndexController.java
@@ -1,23 +1,34 @@
 package com.june.book.springboot.web;
 
+import com.june.book.springboot.config.auth.dto.SessionUser;
 import com.june.book.springboot.service.PostsService;
 import com.june.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null){ //세션이 저장된 값이 있을 때만 저장. 값이 없으면 model에 값 없으니 로그인 버튼이 보임
+            model.addAttribute("userName", user.getName());
+        }
         return "index"; // src/main/resources/templates/index.mustache
     }
 
